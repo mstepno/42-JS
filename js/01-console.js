709f67b148b00edc08504c7f228beca5ad52fbe2
mstepno <121028153+mstepno@users.noreply.github.com>
2023-01-16 12:15:59 +0200

    testing

--- js/01-console.js
+++ js/01-console.js
@@ -1,5 +1,6 @@
-//single-line comment
+console.clear()
 
+//single-line comment
 /*
 multi
 line
@@ -7,3 +8,7 @@ comment
 */
 
 console.log(5);
+console.log(3,4);
+console.log(1,11,11,11)
+console.log(1573745374357373)
+
