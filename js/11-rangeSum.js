c3223438f5313b5002c2f0c1eb7e4376ecf5c294
mstepno <121028153+mstepno@users.noreply.github.com>
2023-01-23 10:16:14 +0200

    reverseString

---
+++ js/11-rangeSum.js
@@ -0,0 +1,16 @@
+console.clear();
+
+function rangeSum(){
+
+    return 0;
+}
+/*
+0 ... 0     0
+0 ... 4     10
+0 ... 100   5050
+574 ... 815   kazkoks didesnis skaicius
+-50 ... 50   0
+-70 ... 30   -2020
+
+*/
+console.log(rangeSum(true, 4));
---
+++ js/13-beliekaninis.js
@@ -0,0 +1,10 @@
+console.clear();
+
+function beliekaninis () {
+return 0;
+
+}
+
+console.log(beliekaninis(0, 11, 3), '-->', 4);
+console.log(beliekaninis(0, 11, 5), '-->', 3);
+console.log(beliekaninis(0, 11, 7), '-->', 2);
